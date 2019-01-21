ab504285ab8ad672547a204fc80f44ca72a7fadc
lukaslukas <eiglerhardingl@gmail.com>
2019-01-21 11:25:12 -0500

    userprofiles recursion, user profile and posts recursion functions added

--- build/network-interfacing.js
+++ build/network-interfacing.js
@@ -1,63 +1,201 @@
 /* NETWORK INTERFACING 
 	- prebuilt functions, 1.0
 
-	available: 
+	available functions: 
 	- loadProfile
-	- loadPosts
+		get profile object
+	- loadPosts 
+		get a list of a user's posts
 	- loadPostContent
+		get content of each post and append
 
+	- loadUsersCentral
+		get a list of users from an external source
+	- usersProfiles
+		use a list of users and return their profile
+	- userAndTheirPosts
+		use a list of users and return their profile + all their posts
+
+	- isOwner
+		check if you are the site's owner
 	- writePost
-
-	in dev:
-	- loadUsers (local && central)
+		submit a post interface
 	
 */
 
 async function loadProfile(archive){
 	var profile = await archive.readFile('/profile.json')
 	profile = JSON.parse(profile)
-	return profile
+
+	var userInfo = {
+		"profile" : profile,
+		"archive" : archive
+	}
+
+	return userInfo
 }
 
 async function loadPosts(archive){
 	var posts = await archive.readdir('/posts/', {stat: true});
-	return posts
+	
+	var userPosts = {
+		"posts" : posts,
+		"archive" : archive
+	}
+
+	return userPosts
 	// should return an posts as json of links/content
 }
 
 async function loadPostContent(archive, post){
 	var postLink = '/posts/' + post.name;
-	console.log(postLink)
+	// console.log(postLink)
 	var postContent = await archive.readFile(postLink)
 	return JSON.parse(postContent)
 }
 
-async function loadUsersLocal(archive){
-	// load users from profile
-	// ideally should be able to load users and then load posts for each user
+async function loadUsersCentral(archive, fileName){
+	var getUserList = await archive.readFile(fileName);
+	getUserList = JSON.parse(getUserList)
+
+	var userList = {
+		"users" : getUserList.users,
+		"archive" : archive
+	}
+
+	return userList;
 }
 
-async function loadUsersCentral(archive){
-	// load users from class list
-	// ideally should be able to load users and then load posts for each user
+function usersProfiles(userCounter, userList, profilesContainer){
+	// recursive function listing all users
+	var userUrl = new DatArchive(userList[userCounter]),
+			userAmount = userList.length;
+
+	loadProfile(userUrl)
+	.then(function(userInfo){
+		var username = userInfo.profile.username,
+				bio = userInfo.profile.bio,
+				email = userInfo.profile.email;
+
+		profilesContainer.insertAdjacentHTML("beforeend", `
+			<li>
+				<hr />
+				<h2>
+					<a href="${userInfo.archive.url}" target="_blank">
+						${username}
+					</a>
+				</h2>
+				<p>${bio}</p>
+				<h3>${email}</h3>
+			</li>
+		`)
+
+		if(userCounter < userAmount - 1){
+			//foreach appending posts for this user is over
+			userCounter++;
+			usersProfiles(userCounter, userList, profilesContainer) // move to next user
+		}
+
+	})
+}
+
+function userAndTheirPosts(userCounter, userList, watchingContainer){
+	// get username from dat url
+	var userUrl = new DatArchive(userList[userCounter]);
+			userAmount = userList.length;
+
+	// load user profile:
+	loadProfile(userUrl)
+	.then(function(userInfo){
+		var username = userInfo.profile.username,
+				bio = userInfo.profile.bio,
+				email = userInfo.profile.email,
+				userId = "user-" + userInfo.archive.url.replace("dat://", ""); // dynamically generated id to be populated later
+
+		watchingContainer.insertAdjacentHTML("beforeend", `
+			<li>
+				<hr />
+				<h2>
+					<a href="${userInfo.archive.url}" target="_blank">
+						${username}
+					</a>
+				</h2>
+				<p>${bio}</p>
+				<h3>${email}</h3>
+				<ul id="${userId}"></ul>
+			</li>
+		`)
+
+		return userInfo.archive;
+
+	})
+	.then(function(userArchive){
+
+		// load posts of user:
+		loadPosts(userArchive)
+		.then(function(userPosts){
+			// this user's posts: 
+			console.log(userPosts)
+			var userId = "user-" + userPosts.archive.url.replace("dat://", ""); // dynamically generated id
+
+			var amountOfPosts = userPosts.posts.length,
+					postCounter = 0;
+
+			userPosts.posts.forEach(function(post){
+				
+				loadPostContent(userUrl, post)
+				.then(function(post){
+					console.log("post\n", post)
+
+					var userPostContainer = document.getElementById(userId) // dynamically generated id
+
+					userPostContainer.insertAdjacentHTML("beforeend", `
+						<li>
+							<hr />
+							<h2>${post.title}</h2>
+							<h4>${post.timestamp}</h4>
+							<p>${post.content}</p>
+						</li>
+					`)
+
+					postCounter++;
+					// console.log(postCounter, amountOfPosts, userCounter, userAmount)
+					if(postCounter >= amountOfPosts && userCounter < userAmount - 1){
+						//foreach appending posts for this user is over
+						userCounter++;
+						userAndTheirPosts(userCounter, userList, watchingContainer) // move to next user
+					}
+				})
+			})
+		}) //end of loadPosts.then
+	})
+} // end of function
+
+
+async function isOwner(archive){
+	var pageInfo = await archive.getInfo();
+	return pageInfo.isOwner;
 }
 
 function writePost(archive, postSubmission){
 	var archive = archive;
 
+	// when someone clicks submit:
 	postSubmission.addEventListener("submit",function(e) {
-  	e.preventDefault(); // before the code
+  	e.preventDefault(); // avoid default behavior
   	var formRecieved = e.target,
   			formTitle = formRecieved.elements["Title"].value.toString(),
   			formContents = formRecieved.elements["Content"].value.toString(),
   			timestamp = new Date().getTime();
 
+  	// set up object to submit to post:
   	var postContent = {
   		"title" : formTitle,
   		"timestamp" : timestamp,
   		"content" : formContents
   	}
 
+  	// use archive (the DatArchive) to write a file
   	async function postFile(archive, postContent){
   		await archive.writeFile('/posts/post-' + postContent.timestamp + '.json', JSON.stringify(postContent));
   	}
