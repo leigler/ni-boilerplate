7691b19c6ccb4f617f816c07d4a311f9baea9ae5
lukaslukas <eiglerhardingl@gmail.com>
2019-01-21 08:05:35 -0500

    write post function

--- build/network-interfacing.js
+++ build/network-interfacing.js
@@ -6,8 +6,10 @@
 	- loadPosts
 	- loadPostContent
 
+	- writePost
+
 	in dev:
-	- loadUsers
+	- loadUsers (local && central)
 	
 */
 
@@ -30,13 +32,42 @@ async function loadPostContent(archive, post){
 	return JSON.parse(postContent)
 }
 
-async function loadUsers(archive){
-	// load users
+async function loadUsersLocal(archive){
+	// load users from profile
 	// ideally should be able to load users and then load posts for each user
 }
 
-async function writeFile(archive, post){
+async function loadUsersCentral(archive){
+	// load users from class list
+	// ideally should be able to load users and then load posts for each user
+}
 
-	await archive.writeFile('/posts/' + post.name + "json", post)
-	return "post accepted";
+function writePost(archive, postSubmission){
+	var archive = archive;
+
+	postSubmission.addEventListener("submit",function(e) {
+  	e.preventDefault(); // before the code
+  	var formRecieved = e.target,
+  			formTitle = formRecieved.elements["Title"].value.toString(),
+  			formContents = formRecieved.elements["Content"].value.toString(),
+  			timestamp = new Date().getTime();
+
+  	var postContent = {
+  		"title" : formTitle,
+  		"timestamp" : timestamp,
+  		"content" : formContents
+  	}
+
+  	async function postFile(archive, postContent){
+  		await archive.writeFile('/posts/post-' + postContent.timestamp + '.json', JSON.stringify(postContent));
+  	}
+
+  	postFile(archive, postContent)
+  	.then(function(event){
+  		console.log("post is posted!")
+  	})
+  	.catch(function(error){
+  		console.log("error\n", error)
+  	})
+	});
 }
